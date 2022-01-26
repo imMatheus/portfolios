c9a2e1d3903779ff05ef3c8f9974a5150d702182
imMatheus <matheusmendesbarata03@gmail.com>
2022-01-26 16:46:30 +0100

    removed bg for navbar

--- modern/components/Navbar.tsx
+++ modern/components/Navbar.tsx
@@ -4,7 +4,7 @@ import SocialLink from './SocialLink'
 
 const Navbar: React.FC = ({}) => {
     return (
-        <nav className='flex p-6 md:px-9 mb-3 bg-white sticky top-0 z-50'>
+        <nav className='flex p-6 md:px-9 mb-3 sticky top-0 z-50'>
             <h2 className='font-mono font-bold text-4xl'>MM</h2>
             <div className='ml-auto flex items-center gap-3'>
                 <SocialLink href='#' Icon={GitHub} />
